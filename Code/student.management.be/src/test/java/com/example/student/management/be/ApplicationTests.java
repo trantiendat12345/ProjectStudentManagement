ca295c9ff873b00f280e6a490367839abd380113
Trần Đạt <trandat@MacBook-Air-cua-Tran.local>
2025-09-25 21:24:50 +0700

    first commit

---
+++ Code/student.management.be/src/test/java/com/example/student/management/be/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student.management.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
