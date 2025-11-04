7d9cb51dde1fb1909f2e14b13292ff27567e5ee6
trantiendat12345 <trantiendatas1@gmail.com>
2025-11-04 22:59:12 +0700

    merge config

--- Code/student.management.be/src/main/java/com/example/student/management/be/configs/SecurityConfig.java
+++ Code/student.management.be/src/main/java/com/example/student/management/be/configs/SecurityConfig.java
@@ -1,12 +1,19 @@
-package com.example.student.management.be.security;
+package com.example.student.management.be.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
